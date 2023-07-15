dc0eb8858316fbed948058293a8a1ce08cfc616a
Alina232323 <alina.polianska@vergesense.com>
2023-07-14 22:18:39 -0700

    test

--- app/models/stock.rb
+++ app/models/stock.rb
@@ -6,6 +6,12 @@ class Stock < ApplicationRecord
             secret_token: Rails.application.credentials.iex_client[:secret_token],
             endpoint: 'https://cloud.iexapis.com/v1'
           )
-        new(ticker: ticker_symbol, name: client.company(ticker_symbol).company_name, last_price: client.price(ticker_symbol))
+
+        begin
+            new(ticker: ticker_symbol, name: client.company(ticker_symbol).company_name, last_price: client.price(ticker_symbol))
+        rescue => exception
+            puts "Exception occurred: #{exception.message}"
+            return nil
+        end
     end
 end
